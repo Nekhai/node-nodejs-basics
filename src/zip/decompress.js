7af3aa482e295b796bb5a90456620226a80f0a87
Nekhai <NekhaiYurii@gmail.com>
2022-06-03 23:22:42 +0300

    feat: add zip-decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,12 @@
+import fs from "fs";
+import { createGunzip } from "zlib";
+
 export const decompress = async () => {
-    // Write your code here 
+  const path = "./files/";
+  const writeStream = fs.createWriteStream(`${path}fileToCompress.txt`);
+  const readStream = fs.createReadStream(`${path}archive.gz`);
+
+  readStream.pipe(createGunzip()).pipe(writeStream);
 };
+
+decompress();
