f12dcb705b33c34601d6163176fd2c69740220c7
Nekhai <NekhaiYurii@gmail.com>
2022-06-03 10:28:03 +0300

    feat: add fs-rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,13 @@
+import fs from "fs";
+
 export const rename = async () => {
-    // Write your code here 
+  fs.rename(
+    "./files/wrongFilename.txt",
+    "./files/properFilename.md",
+    (error) => {
+      if (error) {
+        console.log("FS operation failed");
+      }
+    }
+  );
 };
