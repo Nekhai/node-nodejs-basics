c98a62064499e1742e9f700f141f9b21d9afc9c5
Nekhai <NekhaiYurii@gmail.com>
2022-06-03 14:41:58 +0300

    feat: add cli-args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,8 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const value = process.argv[3];
+  const value2 = process.argv[5];
+
+  console.log(`propName is ${value} prop2Name is ${value2}`);
 };
+
+parseArgs();
