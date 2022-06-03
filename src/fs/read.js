0956d4e5392ef7dcbb9dcda5fd21d4f189e52897
Nekhai <NekhaiYurii@gmail.com>
2022-06-03 11:35:27 +0300

    fix: change the code according to the task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,11 +1,11 @@
 import fs from "fs";
 
 export const read = async () => {
-  fs.readdir("./files", (error, list) => {
+  fs.readFile("./files/fileToRead.txt", "utf8", (error, data) => {
     if (error) {
       console.log("FS operation failed");
     } else {
-      list.forEach((item) => console.log(item));
+      console.log(data);
     }
   });
 };
