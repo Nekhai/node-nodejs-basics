9f2f25c386dc289f6ce1b1836a0367ff8add62be
Nekhai <NekhaiYurii@gmail.com>
2022-06-04 09:05:31 +0300

    feat: add streams-transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,20 @@
+import { Transform, pipeline } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  const terminal = process.stdin;
+  const dest = process.stdout;
+
+  const transformStream = new Transform({
+    transform(chunk, enc, cb) {
+      const reverseChunk = chunk.toString().trim().split("").reverse().join("");
+
+      cb(null, reverseChunk + "\n");
+    },
+  });
+
+  pipeline(terminal, transformStream, dest, (err) => {
+    console.log(err);
+  });
 };
+
+transform();
