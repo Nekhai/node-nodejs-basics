792ee8e0bc494a862b0936288c8b53aea0fb7ef5
Nekhai <NekhaiYurii@gmail.com>
2022-06-05 16:51:33 +0300

    feat: add worker rask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,43 @@
+import os from "os";
+import { Worker } from "worker_threads";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpusCount = os.cpus().length;
+
+  function newWorker(n) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker("./worker.js", {
+        workerData: 10 + n,
+      });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          console.log("error");
+        }
+      });
+    });
+  }
+
+  function arrPromises() {
+    let arr = [];
+
+    for (let i = 0; i < cpusCount; i++) {
+      arr.push(newWorker(i));
+    }
+
+    return arr;
+  }
+
+  const promiseAll = await Promise.allSettled(arrPromises());
+
+  console.log(
+    promiseAll.map((item) =>
+      item.status === "fulfilled"
+        ? { status: "resolved", data: item.value }
+        : { status: "error", data: null }
+    )
+  );
 };
+
+performCalculations();
