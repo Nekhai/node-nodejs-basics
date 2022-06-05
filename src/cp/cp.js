72973323a1d81ba33be1c65ba62d3163bcf55a16
Nekhai <NekhaiYurii@gmail.com>
2022-06-05 18:40:51 +0300

    fis: remove comments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,20 +2,8 @@ import { fork } from "child_process";
 
 export const spawnChildProcess = async (args) => {
   const child = fork("./files/script.js", args);
-
-  // child.stdout.on("data", (data) => {
-  //   console.log("stdout: " + data);
-  // });
-
-  //   child.stderr.on("data", function (data) {
-  //     console.log("stderr: " + data);
-  //   });
-
-  //   child.on("close", function (code) {
-  //     console.log("child process exited with code " + code);
-  //   });
 };
 
-const args = process.argv.slice(2);
+const arg = process.argv.slice(2);
 
-spawnChildProcess(args);
+spawnChildProcess(arg);
