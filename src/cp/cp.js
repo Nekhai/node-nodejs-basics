99ba78c0b0de08845fc3e5316954cd956684f557
Nekhai <NekhaiYurii@gmail.com>
2022-06-05 17:34:27 +0300

    feat: add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,21 @@
+import { fork } from "child_process";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork("./files/script.js", args);
+
+  // child.stdout.on("data", (data) => {
+  //   console.log("stdout: " + data);
+  // });
+
+  //   child.stderr.on("data", function (data) {
+  //     console.log("stderr: " + data);
+  //   });
+
+  //   child.on("close", function (code) {
+  //     console.log("child process exited with code " + code);
+  //   });
 };
+
+const args = process.argv.slice(2);
+
+spawnChildProcess(args);
